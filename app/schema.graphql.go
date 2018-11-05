4d0d927a1407444fae309c24dd974f93575358d2
Arijeet Baruah <arijeet.webointern@gmail.com>
2018-11-05 13:52:20 +0530

    list user (#12)

--- app/schema.graphql.go
+++ app/schema.graphql.go
@@ -13,11 +13,18 @@ import (
 //GraphQlService encapsulates GraphQl functions
 type GraphQlService interface {
 	GetSchema() (graphql.Schema, error)
+	GetApp(app *App)
 }
 
 // GraphQlServiceImpl implement GraphQlService
 type GraphQlServiceImpl struct {
-	DB models.XODB
+	DB  models.XODB
+	App *App
+}
+
+//GetApp save app to GraphQlServiceImpl
+func (serviceImpl *GraphQlServiceImpl) GetApp(app *App) {
+	serviceImpl.App = app
 }
 
 //GetSchema return Schema for GraphQl
@@ -43,18 +50,14 @@ func (serviceImpl *GraphQlServiceImpl) GetSchema() (graphql.Schema, error) {
 				if err != nil {
 					return nil, err
 				}
-				user, err := models.UserByID(serviceImpl.DB, id)
-				if err != nil {
-					return nil, err
-				}
-				return &struct {
-					ID         int
-					Email      string
-					FirstName  string
-					LastName   string
-					ModifiedAt time.Time
-					CreatedAt  time.Time
-				}{user.ID, user.Email, user.FirstName.String, user.LastName.String, user.ModifiedAt, user.CreatedAt}, nil
+				return models.UserByID(serviceImpl.DB, id)
+			},
+		},
+		"users": &graphql.Field{
+			Type:        graphql.NewList(models.UserSchema),
+			Description: "Fetch User by id",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return serviceImpl.App.UserService.GetAllUsers()
 			},
 		},
 		"login": &graphql.Field{
@@ -111,34 +114,33 @@ func (serviceImpl *GraphQlServiceImpl) GetMutation() *graphql.Object {
 						Type: graphql.NewNonNull(graphql.String),
 					},
 					"lastName": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+					"password": &graphql.ArgumentConfig{
 						Type: graphql.NewNonNull(graphql.String),
 					},
 				},
 				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
 					firstName := params.Args["firstName"].(string)
 					lastName := params.Args["lastName"].(string)
+					password, err := bcrypt.GenerateFromPassword([]byte(params.Args["password"].(string)), 10)
+					if err != nil {
+						return nil, err
+					}
 					user := &models.User{
 						Email:      params.Args["email"].(string),
 						FirstName:  sql.NullString{String: firstName, Valid: (firstName == "")},
 						LastName:   sql.NullString{String: lastName, Valid: (lastName == "")},
+						Password:   string(password),
 						ModifiedAt: time.Now(),
 						CreatedAt:  time.Now(),
 					}
 
-					userService := models.UserServiceImpl{DB: serviceImpl.DB}
-					if err := userService.InsertUser(user); err != nil {
+					if err := serviceImpl.App.UserService.InsertUser(user); err != nil {
 						return nil, err
 					}
 
-					return &struct {
-						ID         int
-						Email      string
-						FirstName  string
-						LastName   string
-						ModifiedAt time.Time
-						CreatedAt  time.Time
-					}{user.ID, user.Email, user.FirstName.String, user.LastName.String, user.ModifiedAt, user.CreatedAt}, nil
-
+					return user, nil
 				},
 			},
 		},
